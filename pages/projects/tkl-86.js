043d515595080a3e658f9325a3899a2191753ef7
Alan-JH <alanjhsu08@gmail.com>
2022-12-05 12:09:11 -0500

    tkl-86

--- pages/projects/tkl-86.js
+++ pages/projects/tkl-86.js
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import { img_photo_descrip_block } from '../../components/img_blocks'
 import styles from '../../components/layout.module.css'
 
-const title = "Title"
+const title = "TKL-86 Keyboard"
 
 export default function FirstPost() {
   return (
@@ -19,7 +19,31 @@ export default function FirstPost() {
         <a>Back to projects</a>
       </Link>
       <br></br>
-
+      <p>
+        Right before junior year, I decided to build my own keyboard. I chose Kailh Box Browns for the main keys and Kailh Box Jades for some of the modifier keys.
+        I chose a custom 86 key tenkeyless layout, since I never used number pad and wanted the extra desk space, but also wanted the insert, home, delete, etc. keys.
+        I used <a href="https://github.com/jeroen94704/klepcbgen" target="_blank">KLE PCB Generator</a>&nbsp; to generate the switch layout, then embedded an ATMega32u4
+        IC directly onto the board. 
+      </p>
+      <p>
+        I ordered it and spent about an hour drag soldering the ATMega32u4 on (since this was before I had a hot air station), and another hour soldering all 86 SMD diodes.
+        This was my second time drag soldering, the first time being the <a href="ufc" target="_blank">F-18C UFC</a>. 
+      </p>
+      <center>
+        <img src="/images/keyboard/soldering.jpg"/>
+      </center>
+      <p>
+        I designed up a 3D printed case, and went through several iterations trying to get the tolerance right. I ended up doing a lot of snipping with pliers to get the
+        stabilizers to not get stuck, and the case is a little warped, but it works. I also added in tact switches for media control buttons in the top right. Finally,
+        I generated a firmware in QMK and flashed it. 
+      </p>
+      <center>
+        <img src="/images/keyboard/final.jpg"/>
+        The finished product
+      </center>
+      <p>
+        I still daily drive this keyboard, and it continues to annoy my parents and anyone else who happens to be in the same room while I'm typing.
+      </p>
     </Layout>
 
   )
