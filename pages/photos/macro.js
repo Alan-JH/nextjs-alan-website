c7808f78c453b983e57a75bc9fd3e0d66113f43a
Alan-JH <alanjhsu08@gmail.com>
2022-12-08 22:22:46 -0500

    more macro

--- pages/photos/macro.js
+++ pages/photos/macro.js
@@ -11,6 +11,8 @@ export const images = [
   image("photography/macro", "https://lh3.googleusercontent.com/pw/AL9nZEUxtH3cDI-ckX7Zk_4hXi9mJJi30rdakw0AsRLaqGorCGdGPVRni_TlmMfu5zm_0uivUURNIjUKQyUfPSQ7inuAQ3WubJ_R8NEGanCyJ27GRbNlKblUx_l4xZK8s0S_Pa8BM64NhaquL1qjwOBDSBwEWA=w1463-h975-no?authuser=0", "5.jpg", 1462, 975, "6 Dec 2022 | Super Takumar 50mm f1.4 with 50mm extension | RBF tag"),
   image("photography/macro", "https://lh3.googleusercontent.com/pw/AL9nZEUgsYZy3jFXPm4GHocY8SKvhp6Nh8v2TwG8VI9_7b_HH_BAMWNcFBqvNyWzl3w1FmBRQtuRB9ApSogKhRvCoc9Ct13rZa7aFA7V8OkMYYrJX19adPdIErBBKneLvOCSF1hwGdmH7IHth89hOGLRdxbyKQ=w1463-h975-no?authuser=0", "6.jpg", 1462, 975, "6 Dec 2022 | Super Takumar 50mm f1.4 with 50mm extension | F"),
   image("photography/macro", "https://lh3.googleusercontent.com/pw/AL9nZEVRNZYEIn28gcOjLwG01IpD0urXs6s_o30tl5PWaDJVIUCBBfMOvHbKxYy3h-LWsrrXcpQGWO-AlufYep8mNt54-n7h5fxnWuBarOoQwBrjYb0XwI9lpXa8J2W9M6fi0DkqhDJ7eI2d40eRVS8pbAeEjg=w1463-h975-no?authuser=0", "7.jpg", 1462, 975, "6 Dec 2022 | Super Takumar 50mm f1.4 with 50mm extension | ME Super"),
+  image("photography/macro", "https://lh3.googleusercontent.com/pw/AL9nZEUVzBHaQuQufKoOU3FQ1LgTREGZBMS6zUXOOnZdYM2jB3VXlw4dwov5kOtXOx6zpgnwoQSJoykmzTqZXE-FoETX3K9Ho6dzRmt_XDF85qOTqi7BuKsIstU1BtMG-u3kdlT90_9t9Foifiilx6Wr6l2VSw=w1999-h1336-no?authuser=0", "8.jpg", 1999, 1336, "8 Dec 2022 | Super Takumar 50mm f1.4 with 50mm extension | TPS Regulator"),
+  image("photography/macro", "https://lh3.googleusercontent.com/pw/AL9nZEWzigGsGWHeZpeXbRa6gcdOG_xDehNPDo9dft-ZT2dfaKk6zWsf_QDC_fck6rwG4kjwXuwNeusgSGJEVgZ0hNSUZH7CRIT5eb5rRt_ykuOe6gtkcWFkVkA3ALRWYRhsF4DL7XWsA1O8ofESTDuYkg0kmg=w2004-h1336-no?authuser=0", "9.jpg", 2004, 1336, "8 Dec 2022 | Super Takumar 50mm f1.4 with 50mm extension | Current shunt"),
   
 ];
 
