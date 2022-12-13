2e2d983214e632ade2563340b8a6a8c08ff6e128
Alan-JH <alanjhsu08@gmail.com>
2022-12-12 22:41:14 -0500

    WIP commit

--- pages/projects/reverb-interface.js
+++ pages/projects/reverb-interface.js
@@ -24,7 +24,15 @@ export default function FirstPost() {
       </p>
       <p>
         When the 2022-23 TJREVERB team came in over the summer, the interface board on the flatsat was an old version with incorrect routing corrected with jumper wires. 
-        After spending about a month working with it and getting familiar with the design, 
+        After spending about a month working with it and getting familiar with the design, I got to work on designing and fabricating a flight ready version.
+      </p>
+      <center>
+        <img src="/images/reverb/wiring.jpg"/>
+        The Iridium ribbon cable pinout was wrong, so the previous team used male to female jumper wires to connect it
+      </center>
+
+      <p>
+        WIP Last updated 12 Dec 2022
       </p>
     </Layout>
 
