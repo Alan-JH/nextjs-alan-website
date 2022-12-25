dc2198a8c77ded774959a45c18f7c92182e8b8da
Alan-JH <alanjhsu08@gmail.com>
2022-12-24 22:00:15 -0500

    link on landing page

--- pages/notes.js
+++ pages/notes.js
@@ -10,6 +10,7 @@ export function full_post_list(){
       {notes_list_element_full("How I (Mostly) Made This Website", "how-i-made-this-website", "notes")}
       {notes_list_element_full("On To-do lists", "todo-lists", "notes")}
       {notes_list_element_full("Nostalgic Part Numbers", "part-numbers", "notes")}
+      {notes_list_element_full("Shooting Checklist", "shooting-checklist", "notes")}
     </div>
   }
   
